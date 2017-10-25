ac06ccda25f3c775bc41ffe45a1fac9c8e40bb26
Daniel McClure <daniel@dmsolutionz.co.nz>
2017-10-25 22:06:03 +1100

    development # Added in some simple controls. All good to go.

--- frontend/src/app/material/material.module.ts
+++ frontend/src/app/material/material.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material';
+import { MatSelectModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule
   ],
   exports: [
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class MaterialModule { }
