ae96c94b87d79b73e435cded7d93b6de4b7c124e
Daniel McClure <dmcclure@agl.com.au>
2017-10-26 10:43:36 +1100

    master # added struct.

--- contracts/DRProgram.sol
+++ contracts/DRProgram.sol
@@ -2,30 +2,49 @@ pragma solidity ^0.4.15;
 
 import './token/Kwh.sol';
 
+
 contract DRProgram {
-  uint public constant REWARD_AMOUNT = 1;
+    uint public constant REWARD_AMOUNT = 1;
+    address public rewardsToken_;
 
-  address public rewardsToken_;
-
-  /**
-   * Set the address of the proram reward token upon deployment
-   * @param _kwh The kwh token contract.
-   */
-  function DRProgram(address _kwh) {
-    rewardsToken_ = _kwh;
-  }
-
-  /**
-   * User is claiming their rewards.
-   * @return {[type]} [description]
-   */
-  function claimRewards() {
     /**
-     * TODO
-      Check that the claim is valid!
+     * Contract struct
+     * Enter more information when complete.
      */
+    struct Contract {
+        bool terms;
+        uint256 duration;
+        uint256 payout;
+    }
 
-    // mint will return false if it fails
-    require(Kwh(rewardsToken_).mint(msg.sender, REWARD_AMOUNT));
-  }
+    mapping(bytes32 => Contract) activeContracts_;
+    mapping(address => bytes32) claimsMade_;
+
+    /**
+    * Set the address of the proram reward token upon deployment
+    * @param _kwh The kwh token contract.
+    */
+    function DRProgram(address _kwh) {
+        rewardsToken_ = _kwh;
+    }
+
+    /**
+    * User is claiming their rewards.
+    * @return {[type]} [description]
+    */
+    function claimRewards() {
+        /**
+        * TODO
+          Check that the claim is valid!
+        */
+
+        // mint will return false if it fails
+        require(Kwh(rewardsToken_).mint(msg.sender, REWARD_AMOUNT));
+    }
+
+    /**
+    * kwh reduction
+    * duration
+    * payout Rate - Amounts, Upper Limit & Min
+    */
 }
