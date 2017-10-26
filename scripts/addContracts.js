51f7a8ef276ae54ecbe5c5ba4c0dcb4ca997f26b
Adam J Lemmon <adamjlemmon@gmail.com>
2017-10-26 18:06:00 +1100

    generation script added for events and claims

--- scripts/addContracts.js
+++ scripts/addContracts.js
@@ -8,31 +8,61 @@ const drProgram = contract(DRProgramArtifacts);
 drProgram.setProvider(web3.currentProvider);
 
 const owner = web3.eth.accounts[0]
+let token;
 
 addContracts()
 
 async function addContracts() {
     let nonce = 0;
-    let duration = 10000000;
-    let maxPayout = 1000;
+    let duration;
+    let maxPayout;
+    let energyReduction;
 
-    const token = await drProgram.deployed();
-    const contractParts = [
-      { value: duration, type: 'uint256' },
-      { value: maxPayout, type: 'uint256' },
-      { value: nonce, type: 'uint256' },
-    ];
-    const types = contractParts.map(o => o.type);
-    const values = contractParts.map(o => o.value);
-    const hashBuff = ethABI.soliditySHA3(types, values);
-    const programId = ethUtil.bufferToHex(hashBuff);
+    token = await drProgram.deployed();
 
-    let tx;
-    let energyReduction = 10;
+    energyReduction = Math.floor((Math.random() * 100) + 1)
+    duration = Math.floor((Math.random() * 10000000000000) + 10000000)
+    maxPayout = Math.floor((Math.random() * 1000) + 10)
+    console.log('\n\nEnergy Reduction: ' + energyReduction)
+    console.log('Duration: ' + duration)
+    console.log('Max Payout: ' + maxPayout)
+    await addContractAndClaimReward(nonce++, duration, maxPayout, energyReduction)
 
-    addContractTx = await token.addContract(duration, maxPayout, nonce, {from: owner, gas: 4e6});
-    claimRewardsTx = await token.claimRewards(programId, energyReduction, { from: owner });
+    setInterval(async () => {
+      energyReduction = Math.floor((Math.random() * 100) + 1)
+      duration = Math.floor((Math.random() * 10000000000000) + 10000000)
+      maxPayout = Math.floor((Math.random() * 1000) + 10)
+      console.log('\n\nEnergy Reduction: ' + energyReduction)
+      console.log('Duration: ' + duration)
+      console.log('Max Payout: ' + maxPayout)
+      await addContractAndClaimReward(nonce++, duration, maxPayout, energyReduction)
+    }, Math.floor((Math.random() * 60000) + 10000));
+}
 
-    console.log(addContractTx.logs[0]);
-    console.log(claimRewardsTx.logs[0]);
+async function addContractAndClaimReward(nonce, duration, maxPayout, energyReduction) {
+  const contractParts = [
+    { value: duration, type: 'uint256' },
+    { value: maxPayout, type: 'uint256' },
+    { value: nonce, type: 'uint256' },
+  ];
+  const types = contractParts.map(o => o.type);
+  const values = contractParts.map(o => o.value);
+  const hashBuff = ethABI.soliditySHA3(types, values);
+  const programId = ethUtil.bufferToHex(hashBuff);
+
+  // New Event, add contract
+  addContractTx = await token.addContract(duration, maxPayout, nonce, {from: owner, gas: 4e6});
+  console.log(`New Contract Added! ID: ${programId}`);
+  console.log(addContractTx.logs[0])
+
+  // 30s
+  await sleep(10000)
+
+  claimRewardsTx = await token.claimRewards(programId, energyReduction, { from: owner });
+  console.log('Rewards claimed!');
+  console.log(claimRewardsTx.logs[0])
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
